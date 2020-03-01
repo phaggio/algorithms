9fba032bbee3aedfe83f4c9eaddbfe4fc033b5cf
Richard Wang <richardchacha@hotmail.com>
2020-03-01 14:08:11 -0800

    add test

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// write a function that accepts a number and returns all the fibonacci numbers, in an array, up to that number
+// Fibonacci number: each number is the sum of the two preceding ones
+// 0 is fibonacci number too, no negative
+
+const fibonacci = (num) => {
+
+}
+
+module.exports = fibonacci
---
+++ test/fibonacci.test.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fibonacci = require('../fibonacci');
+
+describe(`fibonacci()`, () => {
+    it(`should return null if takes a non-number value`, () => {
+        const num = 'test';
+        const expected = null;
+        const actual = fibonacci(num);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it(`should return null if passes no parameter to its arg`, () => {
+        const expected = null;
+        const actual = fibonacci();
+
+        expect(actual).toEqual(expected);
+    });
+
+    it(`should return null if takes numeric characters in string`, () => {
+        const num = '30';
+        const expected = null;
+        const actual = fibonacci(num);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it(`should return an array of fibonacci numbers when takes positive number`, () => {
+        const num = 5;
+        const expected = [0, 1, 1, 2, 3, 5];
+        const actual = fibonacci(num);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it(`should return an array of fibonacci numbers when provide number 0`, () => {
+        const num = 0;
+        const expected = [0];
+        const actual = fibonacci(num);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it(`should return an array of fibonacci numbers when provide number 1`, () => {
+        const num = 1;
+        const expected = [0, 1, 1];
+        const actual = fibonacci(num);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it(`should return null when provide a negative number -1`, () => {
+        const num = -1;
+        const expected = null;
+        const actual = fibonacci(num);
+
+        expect(actual).toEqual(expected);
+    });
+});
