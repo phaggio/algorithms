1e291c99f3b56818877020c21c4f2fb32e5d059f
Richard Wang <richardchacha@hotmail.com>
2020-03-01 18:52:45 -0800

    update array-callback2 test

--- array-callback2.js
+++ array-callback2.js
@@ -1,25 +1,30 @@
 'use strict';
 
 // Write the below four functions, each of which has two parameters - an array and a function which returns a boolean given an entry in the array
-
 // ## HasSome
-
 // Returns true if any of the values in the array meet the given condition
-
 // ## HasNone
-
 // Returns true if none of the values in the array meet the given condition
-
 // ## HasAll
-
 // Returns true if all of the values in the array meet the given condition
-
 // ## HasMost
-
 // Returns true if more than 50% of the values in the array meet the given condition
 
-const arrayCallback2 = (arr, cb) => {
+const cb = (num) => num % 2 === 0;
 
+const arrayCallback2 = {
+    hasSome: function (array, cb) {
+
+    },
+    hasNone: function (array, cb) {
+
+    }, 
+    hasAll: function (array, cb) {
+
+    }, 
+    hasMost: function (array, cb) {
+
+    }
 }
 
 module.exports = arrayCallback2
--- test/array-callback2.test.js
+++ test/array-callback2.test.js
@@ -1,106 +1,111 @@
 'use strict';
 
-const drill = require('../drill-solution');
+const arrayCallback2 = require('../array-callback2');
 
-describe('has', () => {
+describe('arrayCallback2()', () => {
+
   const isEven = num => num % 2 === 0;
+
 
   describe('hasSome', () => {
     it('should verify when a value matches', () => {
       const input = [1, 2, 3];
       const expected = true;
 
-      expect(drill.hasSome(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasSome(input, isEven)).toEqual(expected);
     });
 
     it('should verify when a value does not match', () => {
       const input = [1, 3];
       const expected = false;
 
-      expect(drill.hasSome(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasSome(input, isEven)).toEqual(expected);
     });
 
     it('should handle non-arrays', () => {
       const input = null;
       const expected = false;
 
-      expect(drill.hasSome(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasSome(input, isEven)).toEqual(expected);
     });
   });
+
 
   describe('hasNone', () => {
     it('should verify when a value matches', () => {
       const input = [1, 2, 3];
       const expected = false;
 
-      expect(drill.hasNone(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasNone(input, isEven)).toEqual(expected);
     });
 
     it('should verify when none match', () => {
       const input = [1, 3];
       const expected = true;
 
-      expect(drill.hasNone(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasNone(input, isEven)).toEqual(expected);
     });
 
     it('should handle non-arrays', () => {
       const input = null;
       const expected = true;
 
-      expect(drill.hasNone(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasNone(input, isEven)).toEqual(expected);
     });
   });
+
 
   describe('hasAll', () => {
     it('should verify when all values match', () => {
       const input = [2, 4, 8, 16];
       const expected = true;
 
-      expect(drill.hasAll(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasAll(input, isEven)).toEqual(expected);
     });
 
     it('should verify when no values match', () => {
       const input = [1, 3, 5, 7, 11];
       const expected = false;
 
-      expect(drill.hasAll(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasAll(input, isEven)).toEqual(expected);
     });
 
     it('should handle non-arrays', () => {
       const input = null;
       const expected = false;
 
-      expect(drill.hasAll(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasAll(input, isEven)).toEqual(expected);
     });
   });
+
 
   describe('hasMost', () => {
     it('should verify when most match', () => {
       const input = [1, 2, 3, 4, 6, 8];
       const expected = true;
 
-      expect(drill.hasMost(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasMost(input, isEven)).toEqual(expected);
     });
 
     it('should verify when most do not match', () => {
       const input = [1, 2, 3];
       const expected = false;
 
-      expect(drill.hasMost(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasMost(input, isEven)).toEqual(expected);
     });
 
     it('should verify when none match', () => {
       const input = [1, 3];
       const expected = false;
 
-      expect(drill.hasMost(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasMost(input, isEven)).toEqual(expected);
     });
 
     it('should handle non-arrays', () => {
       const input = null;
       const expected = false;
 
-      expect(drill.hasMost(input, isEven)).toEqual(expected);
+      expect(arrayCallback2.hasMost(input, isEven)).toEqual(expected);
     });
   });
 });
