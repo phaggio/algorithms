70d9e58dcd2df1a7fb63dad64911250581bc4df7
phaggio <richardchacha@hotmail.com>
2020-06-22 21:43:52 -0700

    update

--- bootcamp/fibonacci.js
+++ bootcamp/fibonacci.js
@@ -3,7 +3,14 @@
 // 0 is fibonacci number too, no negative
 
 const fibonacci = num => {
-
+  if (num === null || typeof num !== 'number' || num < 0) return null;
+  if (num === 0) return [0];
+  const result = [0, 1, 1];
+  while (result[result.length - 1] !== num) {
+    const newNum = result[result.length -2] + result[result.length - 1];
+    result.push(newNum);
+  }
+  return result;
 }
 
 export default fibonacci
