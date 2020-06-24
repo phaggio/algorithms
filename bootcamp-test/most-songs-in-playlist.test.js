69a168fe5fffe10b005170a142646d5945b04931
phaggio <richardchacha@hotmail.com>
2020-06-24 00:27:31 -0700

    update

--- bootcamp-test/most-songs-in-playlist.test.js
+++ bootcamp-test/most-songs-in-playlist.test.js
@@ -1,54 +1,55 @@
-'use strict';
+import mostSongsInPlaylist from '../bootcamp/most-songs-in-playlist';
 
-const expect = chai.expect;
+describe('mostSongsInPlaylist', () => {
+	it('should return 19 when given the array [3, 5, 7, 2, 1, 4, 8, 5, 3, 2, 2, 1, 5, 7, 4, 2, 3, 1, 3, 4, 7, 8, 8, 2]', () => {
+		const arr = [
+			3,
+			5,
+			7,
+			2,
+			1,
+			4,
+			8,
+			5,
+			3,
+			2,
+			2,
+			1,
+			5,
+			7,
+			4,
+			2,
+			3,
+			1,
+			3,
+			4,
+			7,
+			8,
+			8,
+			2
+		];
 
-describe('mostSongsInPlaylist', function() {
-  it('should return 19 when given the array [3, 5, 7, 2, 1, 4, 8, 5, 3, 2, 2, 1, 5, 7, 4, 2, 3, 1, 3, 4, 7, 8, 8, 2]', function() {
-    const arr = [
-      3,
-      5,
-      7,
-      2,
-      1,
-      4,
-      8,
-      5,
-      3,
-      2,
-      2,
-      1,
-      5,
-      7,
-      4,
-      2,
-      3,
-      1,
-      3,
-      4,
-      7,
-      8,
-      8,
-      2
-    ];
+		const expected = 19;
+		const actual = mostSongsInPlaylist(arr);
 
-    const result = mostSongsInPlaylist(arr);
+		expect(actual).toEqual(expected);
+	});
 
-    expect(result).to.eql(19);
-  });
+	it('should return 7 when given the array [3, 4, 2, 1, 7, 5, 3]', () => {
+		const arr = [3, 4, 2, 1, 7, 5, 3];
 
-  it('should return 7 when given the array [3, 4, 2, 1, 7, 5, 3]', function() {
-    const arr = [3, 4, 2, 1, 7, 5, 3];
+		const expected = 7;
+		const actual = mostSongsInPlaylist(arr);
 
-    const result = mostSongsInPlaylist(arr);
+		expect(actual).toEqual(expected);
+	});
 
-    expect(result).to.eql(7);
-  });
+	it('should return 0 when given the array []', () => {
+		const arr = [];
 
-  it('should return 0 when given the array []', function() {
-    const arr = [];
+		const expected = 0;
+		const actual = mostSongsInPlaylist(arr);
 
-    const result = mostSongsInPlaylist(arr);
-
-    expect(result).to.eql(0);
-  });
+		expect(actual).toEqual(expected);
+	});
 });
