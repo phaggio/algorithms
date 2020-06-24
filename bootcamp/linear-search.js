2b641baaff64b29903841b388ae0c4c6ecc7ef49
phaggio <richardchacha@hotmail.com>
2020-06-23 21:00:24 -0700

    update

--- bootcamp/linear-search.js
+++ bootcamp/linear-search.js
@@ -4,7 +4,10 @@
 // You may not use the `indexOf`, `lastIndexOf` or `includes` methods
 
 const linearSearch = (arr, target) => {
-
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
 };
 
 export default linearSearch
