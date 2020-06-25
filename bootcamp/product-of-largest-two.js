d952c457684010fcd2f6821578b150515be29307
phaggio <richardchacha@hotmail.com>
2020-06-25 01:39:02 -0700

    update

--- bootcamp/product-of-largest-two.js
+++ bootcamp/product-of-largest-two.js
@@ -2,7 +2,18 @@
 // finds the largest two numbers, and returns the product of the two
 
 const productOfLargestTwo = arr => {
+	let numOne = null;
+	let numTwo = null;
 
+	for (const num of arr) {
+		if (num >= numOne || numOne === null) {
+			numTwo = numOne;
+			numOne = num;
+		} else if (num > numTwo || numTwo === null) {
+			numTwo = num;
+		}
+	}
+	return numOne * numTwo;
 };
 
 export default productOfLargestTwo
