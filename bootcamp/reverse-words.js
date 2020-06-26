1a8426db8313afac626cfba67ca7951f7cf775c3
phaggio <richardchacha@hotmail.com>
2020-06-26 13:12:51 -0700

    update

--- bootcamp/reverse-words.js
+++ bootcamp/reverse-words.js
@@ -4,7 +4,13 @@
 
 
 const reverseWords = str => {
-
+  if (!str) return '';
+  const arr = str.split(` `);
+  const reversedArr = [];
+  for (let i = arr.length - 1; i >= 0; --i) {
+    reversedArr.push(arr[i]);
+  };
+  return reversedArr.join(` `);
 };
 
 export default reverseWords
