6856d63c2d996042314146d82c1c02d7a748795d
phaggio <richardchacha@hotmail.com>
2020-06-26 13:09:33 -0700

    update

--- bootcamp/reverse-in-place.js
+++ bootcamp/reverse-in-place.js
@@ -2,7 +2,13 @@
 // You may NOT use the built-in reverse method
 
 const reverseInPlace = str => {
-
+  if (!str) return '';
+  const arr = str.split(``);
+  let newStr = ``;
+  for (let i = str.length - 1; i >= 0; --i) {
+    newStr += arr[i];
+  }
+  return newStr;
 };
 
 export default reverseInPlace
