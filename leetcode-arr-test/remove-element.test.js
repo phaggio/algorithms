b7f6b30d79eae9ecd29bc2ac09c06689b45535bc
phaggio <richardchacha@hotmail.com>
2020-06-26 23:33:00 -0700

    update

---
+++ leetcode-arr-test/remove-element.test.js
@@ -0,0 +1,23 @@
+import removeElement from '../leetcode-arr/remove-element';
+
+describe('removeElement()', () => {
+  it(`should return count of 3 for ([1, 2, 3, 3, 4], 3)`, () => {
+    const arr = [1, 2, 3, 3, 4];
+    const val = 3;
+    const expected = 3;
+    const actual = removeElement(arr, val);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should return count of 4 for ([1, 2, 0, 2, 2, 0], 0)`, () => {
+    const arr = [1, 2, 0, 2, 2, 0];
+    const val = 0;
+    const expected = 4;
+    const actual = removeElement(arr, val);
+
+    expect(actual).toEqual(expected);
+  });
+
+
+});
---
+++ leetcode-arr/remove-element.js
@@ -0,0 +1,22 @@
+// Given an array nums and a value val, 
+// remove all instances of that value in-place and return the new length.
+
+// Do not allocate extra space for another array, 
+// you must do this by modifying the input array in-place with O(1) extra memory.
+
+// e.g. nums = [3, 2, 2, 3, 3] val = 3 return 2
+// where result array is [2, 2]
+
+const removeElement = (nums, val) => {
+  let length = nums.length;
+  for (let i = 0; i < length; ++i) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      length--;
+      i--;
+    }
+  }
+  return nums.length;
+}
+
+export default removeElement
