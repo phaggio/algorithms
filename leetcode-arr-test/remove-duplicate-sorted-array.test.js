a30b09a3b976f04ef3f593579384d0d23dc24107
phaggio <richardchacha@hotmail.com>
2020-06-26 23:17:11 -0700

    update

--- leetcode-arr-test/remove-duplicate-sorted-array.test.js
+++ leetcode-arr-test/remove-duplicate-sorted-array.test.js
@@ -1,7 +1,7 @@
 import removeDup from '../leetcode-arr/remove-duplicate-sorted-array';
 
 describe('removeDup()', () => {
-  it(`should remove duplicate in the sorted arr and return its length [1, 2, 3, 3, 4]`, () => {
+  it(`should count of unique numbers in sorted arr [1, 2, 3, 3, 4]`, () => {
     const arr = [1, 2, 3, 3, 4];
     const expected = 4;
     const actual = removeDup(arr);
@@ -16,5 +16,21 @@ describe('removeDup()', () => {
 
     expect(actual).toEqual(expected);
   });
+
+  // it(`should count of unique numbers in sorted arr [1, 2, 3, 3, 4]`, () => {
+  //   const arr = [1, 2, 3, 3, 4];
+  //   const expected = [1, 2, 3, 4];
+  //   const actual = removeDup(arr);
+
+  //   expect(actual).toEqual(expected);
+  // });
+
+  // it(`should return 5 for [0,0,1,1,1,2,2,3,3,4]`, () => {
+  //   const arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+  //   const expected = [0, 1, 2, 3, 4];
+  //   const actual = removeDup(arr);
+
+  //   expect(actual).toEqual(expected);
+  // });
 
 });
--- leetcode-arr/remove-duplicate-sorted-array.js
+++ leetcode-arr/remove-duplicate-sorted-array.js
@@ -4,12 +4,30 @@
 // Do not allocate extra space for another array, 
 // you must do this by modifying the input array in-place with O(1) extra memory.
 
-const removeDup = arr => {
-  let count = 1;
-  for (let i = 1; i < arr.length; ++i) {
-    if (arr[i] !== arr[i - 1]) { count++ };
-  };
-  return count;
+// const removeDup = nums => {
+//   let count = 1;
+//   let uniqueIndex = 0;
+//   for (let i = 0; i < nums.length; ++i) {
+//     if (nums[uniqueIndex] !== nums[i]) {
+//       uniqueIndex = i;
+//       count++
+//     };
+//   };
+//   return count;
+// }
+
+const removeDup = nums => {
+  let i = 0;
+  let endIndex = nums.length - 1;
+  while (i < endIndex) {
+    if (nums[i] === nums[i + 1]) { 
+      nums.splice(i + 1, 1);
+      endIndex--;
+    } else {
+      i++;
+    }
+  }
+  return nums.length;
 }
 
 export default removeDup
