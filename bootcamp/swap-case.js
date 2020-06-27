ed7e69db2d58475a5b9ffc007dacf1419f7deeb0
phaggio <richardchacha@hotmail.com>
2020-06-26 21:35:37 -0700

    update

--- bootcamp/swap-case.js
+++ bootcamp/swap-case.js
@@ -2,8 +2,72 @@
 // assume it doesn't take special characters
 // it can take numbers in string
 
-const swapCase = str => {
+const caseMap = {
+  'A': 'a',
+  'B': 'b',
+  'C': 'c',
+  'D': 'd',
+  'E': 'e',
+  'F': 'f',
+  'G': 'g', 
+  'H': 'h',
+  'I': 'i',
+  'J': 'j',
+  'K': 'k',
+  'L': 'l',
+  'M': 'm',
+  'N': 'n',
+  'O': 'o', 
+  'P': 'p',
+  'Q': 'q',
+  'R': 'r',
+  'S': 's',
+  'T': 't',
+  'U': 'u',
+  'V': 'v',
+  'W': 'w',
+  'X': 'x',
+  'Y': 'y',
+  'Z': 'z',
+  'a': 'A',
+  'b': 'B',
+  'c': 'C',
+  'd': 'D',
+  'e': 'E',
+  'f': 'F',
+  'g': 'G', 
+  'h': 'H',
+  'i': 'I',
+  'j': 'J',
+  'k': 'K',
+  'l': 'L',
+  'm': 'M',
+  'n': 'N',
+  'o': 'O', 
+  'p': 'P',
+  'q': 'Q',
+  'r': 'R',
+  's': 'S',
+  't': 'T',
+  'u': 'U',
+  'v': 'V',
+  'w': 'W',
+  'x': 'X',
+  'y': 'Y',
+  'z': 'Z',
+};
 
+const swapCase = str => {
+  const arr = str.split(``);
+  const newArr = [];
+  for (const char of arr) {
+    if (caseMap[char]) {
+      newArr.push(caseMap[char]);
+    } else {
+      newArr.push(char);
+    }
+  };
+  return newArr.join(``);
 };
 
 export default swapCase
