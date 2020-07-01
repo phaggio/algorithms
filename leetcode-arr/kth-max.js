77fdea7c9743bb4cccbc7fdfc43e0b31859e2af3
phaggio <richardchacha@hotmail.com>
2020-07-01 00:13:40 -0700

    update

--- leetcode-arr/kth-max.js
+++ leetcode-arr/kth-max.js
@@ -3,9 +3,39 @@
 // not the kth distinct element.
 
 
+const mergeSort = arr => {
+  if (arr.length === 1) return arr;
+
+  // divide the array in half
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  let sortedArr = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] > right[rightIndex]) {
+      sortedArr.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      sortedArr.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  // concat the last remaining element
+  sortedArr = sortedArr.concat(left.slice(leftIndex));
+  sortedArr = sortedArr.concat(right.slice(rightIndex));
+  return sortedArr;
+};
+
 const findKthLargest = (nums, k) => {
-
-
+  // sort first
+  const sortedArr = mergeSort(nums);
+  // return the value at kth index
+  return sortedArr[k - 1];
 }
 
 export default findKthLargest
