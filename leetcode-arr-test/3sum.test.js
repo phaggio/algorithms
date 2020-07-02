1e97f857b02e0d3a5fd1d12aa45382c06ab06515
phaggio <richardchacha@hotmail.com>
2020-07-01 18:22:34 -0700

    update

--- leetcode-arr-test/3sum.test.js
+++ leetcode-arr-test/3sum.test.js
@@ -9,7 +9,7 @@ describe('threeSum()', () => {
     ]
     for [-1, 0, 1, 2, -1, -4]`, () => {
     const nums = [-1, 0, 1, 2, -1, -4];
-    const expected = [[-1, 0, 1], [-1, -1, 2]];
+    const expected = [[-1, -1, 2], [-1, 0, 1]];
     const actual = threeSum(nums);
 
     expect(actual).toEqual(expected);
@@ -70,6 +70,31 @@ describe('threeSum()', () => {
     for []`, () => {
     const nums = [];
     const expected = [];
+    const actual = threeSum(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return
+    [
+      [-4,-1,5],
+      [-3,-2,5],
+      [-3,1,2],
+      [-2,0,2],
+      [-1,-1,2],
+      [-1,0,1]
+    ]
+    for [-1,0,1,2,-1,-4,-2,-3,5]`, () => {
+    const nums = [-1, 0, 1, 2, -1, -4, -2, -3, 5];
+    const expected = [
+      [-4, -1, 5],
+      [-3, -2, 5],
+      [-3, 1, 2],
+      [-2, 0, 2],
+      [-1, -1, 2],
+      [-1, 0, 1]
+    ];
     const actual = threeSum(nums);
 
     expect(actual).toEqual(expected);
--- leetcode-arr/3sum.js
+++ leetcode-arr/3sum.js
@@ -5,8 +5,48 @@
 // Note:
 // The solution set must not contain duplicate triplets.
 
+// firstInt + secondInt + thirdInt = 0;
+// thirdInt = 0 - firstInt - secondInt;
+
 const threeSum = nums => {
+  if (nums.length < 3) return [];
+  nums.sort((a, b) => a - b); // ascending sort
+
+  const result = [];
+
+  for (let currentIndex = 0; currentIndex < nums.length; ++currentIndex) {
+    let nextIndex = currentIndex + 1;
+    let lastIndex = nums.length - 1;
+
+    while (nextIndex < lastIndex) {
+      const sum = nums[currentIndex] + nums[nextIndex] + nums[lastIndex];
+      if (sum === 0) {
+        result.push([nums[currentIndex], nums[nextIndex], nums[lastIndex]]);
+
+        while (nextIndex < lastIndex && nums[nextIndex] === nums[nextIndex + 1]) nextIndex++;
+        nextIndex++;
+        while (nextIndex < lastIndex && nums[lastIndex] === nums[lastIndex - 1]) lastIndex--;
+        lastIndex--;
+
+      } else if (sum < 0) {
+        while (nextIndex < lastIndex && nums[nextIndex] === nums[nextIndex + 1]) nextIndex++;
+        nextIndex++;
+      } else {
+        while (nextIndex < lastIndex && nums[lastIndex] === nums[lastIndex - 1]) lastIndex--;
+        lastIndex--;
+      }
+    }
+
+    while (currentIndex < nums.length - 1 && nums[currentIndex] === nums[currentIndex + 1]) currentIndex++;
+
+  }
+  return result
 
 }
 
 export default threeSum
+
+// const arr = [-1, 0, 1, 2];
+// threeSum(arr);
+
+// Two pointers
