087308410e7d604e4fb513f67a47656b13788207
phaggio <richardchacha@hotmail.com>
2020-07-03 22:29:39 -0700

    improve next-permutation

--- leetcode-arr-test/next-permutation.test.js
+++ leetcode-arr-test/next-permutation.test.js
@@ -2,22 +2,134 @@ import nextPermutation from '../leetcode-arr/next-permutation.js';
 
 describe('nextPermutation()', () => {
   it(
-    `should return 2 for
-      [
-        [1, 0, 1],
-        [0, -2, 3]
-      ]
-      k = 2
+    `should return [0] for [0]
     `, () => {
-    const matrix = [
-      [1, 0, 1],
-      [0, -2, 3]
-    ];
-    const k = 2;
-    const expected = 2;
-    const actual = nextPermutation(matrix, k);
+    const nums = [0]
+    const expected = [0];
+    const actual = nextPermutation(nums);
 
     expect(actual).toEqual(expected);
   });
+
+  it(
+    `should return [2,1,3] for [1,3,2]
+    `, () => {
+    const nums = [1, 3, 2]
+    const expected = [2, 1, 3];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [1,3,2] for [1,2,3]
+    `, () => {
+    const nums = [1, 2, 3]
+    const expected = [1, 3, 2];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [2,1,3,4] for [1,4,3,2]
+    `, () => {
+    const nums = [1, 4, 3, 2]
+    const expected = [2, 1, 3, 4];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [2,1,1,3,4] for [1, 4, 3, 2, 1]
+    `, () => {
+    const nums = [1, 4, 3, 2, 1]
+    const expected = [2, 1, 1, 3, 4];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [2,1,1,1,2,3,4] for [1, 4, 3, 2, 2, 1, 1]
+    `, () => {
+    const nums = [1, 4, 3, 2, 2, 1, 1];
+    const expected = [2, 1, 1, 1, 2, 3, 4];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [1,3,2] for [1,2,3]
+    `, () => {
+    const nums = [1, 2, 3]
+    const expected = [1, 3, 2];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [1,2,3] for [3,2,1]
+    `, () => {
+    const nums = [3, 2, 1]
+    const expected = [1, 2, 3];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [1,5,1] for [1,1,5]
+    `, () => {
+    const nums = [1, 1, 5]
+    const expected = [1, 5, 1];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [0,0,1] for [1,0,0]
+    `, () => {
+    const nums = [1, 0, 0]
+    const expected = [0, 0, 1];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [1,0,0] for [0,1,0]
+    `, () => {
+    const nums = [0, 1, 0]
+    const expected = [1, 0, 0];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [0,1,0] for [0,0,1]
+    `, () => {
+    const nums = [0, 0, 1]
+    const expected = [0, 1, 0];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(
+    `should return [8,8,7,6,3,1,1,2,2,4,5] for [8,8,7,6,2,5,4,3,2,2,1,0]
+    `, () => {
+    const nums = [8, 8, 7, 6, 2, 5, 4, 3, 2, 2, 1, 0];
+    const expected = [8, 8, 7, 6, 3, 0, 1, 2, 2, 2, 4, 5];
+    const actual = nextPermutation(nums);
+
+    expect(actual).toEqual(expected);
+  });
+
 
 });
--- leetcode-arr/next-permutation.js
+++ leetcode-arr/next-permutation.js
@@ -10,7 +10,48 @@
 
 
 const nextPermutation = nums => {
+  if (nums.length === 1) return nums;
 
+  // swap two elements in the array
+  const swap = (i, j) => {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
+  // reverse the elements in an array starting startIndex till end of the array
+  const reverse = (start, end) => {
+    while (start < end) {
+      swap(start, end);
+      start++;
+      end--;
+    }
+  }
+
+  // start from the last el in array
+  for (let i = nums.length - 1; i > 0; --i) {
+
+    // check against its previous digit
+    if (nums[i] > nums[i - 1]) {
+      // reverse the subarray from index i to end of array, to ascending order.
+      reverse(i, nums.length - 1);
+
+      for (let k = i; k < nums.length; ++k) {
+        if (nums[i - 1] < nums[k]) {
+          swap(k, i - 1) 
+          // break;
+          return nums;
+        }
+      }
+
+      // return nums;
+    }
+  };
+  reverse(0, nums.length - 1);
+  return nums;
 }
 
 export default nextPermutation
+
+
+// O of n log n
