7218975cd7ada7a83a3225d3f065894af3053437
phaggio <richardchacha@hotmail.com>
2020-07-06 00:17:06 -0700

    add test

--- leetcode-linkedlist-test/add-two-numbers.test.js
+++ leetcode-linkedlist-test/add-two-numbers.test.js
@@ -12,5 +12,25 @@ describe('addTwoNumbers()', () => {
     expect(actual).toEqual(expected);
   });
 
+  it(`should return new LinkedList [7,0,8,1,1]`, () => {
+    const list1 = new LinkedList([2, 4, 3, 1, 1]);
+    const list2 = new LinkedList([5, 6, 4]);
+
+    const expected = new LinkedList([7, 0, 8, 1, 1])
+    const actual = addTwoNumbers(list1, list2);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should return new LinkedList [7,7,3,1]`, () => {
+    const list1 = new LinkedList([2, 1, 9]);
+    const list2 = new LinkedList([5, 6, 4]);
+
+    const expected = new LinkedList([7, 7, 3, 1])
+    const actual = addTwoNumbers(list1, list2);
+
+    expect(actual).toEqual(expected);
+  });
+
 });
 
