83756fa887ace357d10a06608fe991f50e3670d9
phaggio <richardchacha@hotmail.com>
2020-07-06 18:50:10 -0700

    add test

--- leetcode-linkedlist-test/merge-two-sorted-lists.test.js
+++ leetcode-linkedlist-test/merge-two-sorted-lists.test.js
@@ -11,4 +11,22 @@ describe('mergeTwoLists()', () => {
     expect(actual).toEqual(expected);
   });
 
+  it(`should return new LinkedList [1, 1, 1, 2, 3, 4, 4, 6, 7]`, () => {
+    const list1 = new LinkedList([1, 1, 2, 4, 6, 7]);
+    const list2 = new LinkedList([1, 3, 4]);
+
+    const expected = new LinkedList([1, 1, 1, 2, 3, 4, 4, 6, 7])
+    const actual = mergeTwoLists(list1, list2);
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should return new LinkedList [0, 0, 1, 1, 2, 4]`, () => {
+    const list1 = new LinkedList([0, 0, 1, 2, 4]);
+    const list2 = new LinkedList([1]);
+
+    const expected = new LinkedList([0, 0, 1, 1, 2, 4])
+    const actual = mergeTwoLists(list1, list2);
+    expect(actual).toEqual(expected);
+  });
+
 });
