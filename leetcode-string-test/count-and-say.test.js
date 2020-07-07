079e64e159193b3f83a1a0d66999d363397d3c65
phaggio <richardchacha@hotmail.com>
2020-07-07 16:55:30 -0700

    count and say

---
+++ leetcode-string-test/count-and-say.test.js
@@ -0,0 +1,60 @@
+import countAndSay from '../leetcode-string/count-and-say';
+
+describe('countAndSay()', () => {
+  it(`should return new string '1' for input of 1`, () => {
+    const n = 1;
+    const expected = '1';
+    const actual = countAndSay(n);
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(`should return new string '11' for input of 2`, () => {
+    const n = 2;
+    const expected = '11';
+    const actual = countAndSay(n);
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(`should return new string '21' for input of 3`, () => {
+    const n = 3;
+    const expected = '21';
+    const actual = countAndSay(n);
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(`should return new string '1211' for input of 4`, () => {
+    const n = 4;
+    const expected = '1211';
+    const actual = countAndSay(n);
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(`should return new string '111221' for input of 5`, () => {
+    const n = 5;
+    const expected = '111221';
+    const actual = countAndSay(n);
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(`should return new string '21' for input of 9`, () => {
+    const n = 9;
+    const expected = '31131211131221';
+    const actual = countAndSay(n);
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(`should return new string '11131221133112132113212221' for input of 11`, () => {
+    const n = 11;
+    const expected = '11131221133112132113212221';
+    const actual = countAndSay(n);
+    expect(actual).toEqual(expected);
+  });
+
+
+});
---
+++ leetcode-string/count-and-say.js
@@ -0,0 +1,34 @@
+// The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+
+
+const countAndSay = n => {
+  let str = '1';
+  for (let i = 1; i < n; ++i) {
+    let currentStr = '';
+    let currentChar = str[0];
+    let count = 0;
+
+    for (const char of str) {
+      if (char === currentChar) {
+        count++;
+      } else {
+        currentStr = `${currentStr}${count}${currentChar}`;
+        currentChar = char;
+        count = 1;
+      }
+    }
+    currentStr = `${currentStr}${count}${currentChar}`;
+    str = currentStr;
+    currentStr = '';
+  }
+
+  return str;
+}
+
+export default countAndSay
