c805cd057227f2544048e34e5ec22c434e36b22e
phaggio <richardchacha@hotmail.com>
2020-07-08 20:33:19 -0700

    symmetric tree

---
+++ leetcode-bfs-test/symmetric-tree.test.js
@@ -0,0 +1,57 @@
+import isSymmetric from '../leetcode-bfs/symmetric-tree';
+import { insertLevelOrder } from '../data-structure/BinaryTreeNode';
+
+describe('isSymmetric()', () => {
+  it(
+    `should true for:
+      []
+    `, () => {
+    const root = insertLevelOrder([], null, 0);
+
+    const expected = true;
+    const actual = isSymmetric(root);
+
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(
+    `should true for:
+      [1]
+    `, () => {
+    const root = insertLevelOrder([1], 1, 0);
+
+    const expected = true;
+    const actual = isSymmetric(root);
+
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(
+    `should true for:
+      [1,2,2]
+    `, () => {
+    const root = insertLevelOrder([1, 2, 2], 1, 0);
+
+    const expected = true;
+    const actual = isSymmetric(root);
+
+    expect(actual).toEqual(expected);
+  });
+
+
+  it(
+    `should false for:
+      [1,2,2,null,3,null,3]
+    `, () => {
+    const root = insertLevelOrder([1, 2, 2, null, 3, null, 3], 1, 0);
+
+    const expected = false;
+    const actual = isSymmetric(root);
+
+    expect(actual).toEqual(expected);
+  });
+
+
+});
---
+++ leetcode-bfs/symmetric-tree.js
@@ -0,0 +1,48 @@
+// #101
+
+// Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+// For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+//     1
+//    / \
+//   2   2
+//  / \ / \
+// 3  4 4  3
+
+// But the following [1,2,2,null,3,null,3] is not:
+
+//    1
+//   / \
+//  2   2
+//   \   \
+//    3   3
+
+// iterative solution
+const isSymmetric = root => {
+  if (!root || (root.left === null && root.right === null)) return true;
+
+  const queue = [root.left, root.right];
+
+  while (queue.length !== 0) {
+    let left = queue.shift();
+    let right = queue.pop();
+    if (left === null && right === null) continue;
+    if (left === null && right !== null) return false;
+    if (left !== null && right === null) return false;
+
+    if (left.val !== right.val) return false;
+
+    queue.unshift(left.right);
+    queue.push(right.left);
+    queue.unshift(left.left);
+    queue.push(right.right);
+  }
+
+  return true;
+}
+
+export default isSymmetric
+
+// time 2^root
+// shift is O(n)
+// pop is O(1)
