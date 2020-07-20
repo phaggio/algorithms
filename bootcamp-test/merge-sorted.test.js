98d882a5e2e9bdaeeeb45fc803c2003e01a7ef66
phaggio <richardchacha@hotmail.com>
2020-07-19 22:28:50 -0700

    merge-sorted

--- bootcamp-test/merge-sorted.test.js
+++ bootcamp-test/merge-sorted.test.js
@@ -11,9 +11,36 @@ describe(`mergeSorted()`, () => {
 	});
 
 	it(`should return a sorted array of negative numbers`, () => {
-		const arr1 = [-1, -3, -5, -7, -9];
-		const arr2 = [-2, -4, -6, -8, -10];
+		const arr1 = [-9, -7, -5, -3, -1];
+		const arr2 = [-10, -8, -6, -4, -2];
 		const expected = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1];
+		const actual = mergeSorted(arr1, arr2);
+
+		expect(actual).toEqual(expected);
+	});
+
+	it(`should return a sorted array when one array is empty`, () => {
+		const arr1 = [1, 3, 5, 7, 9];
+		const arr2 = [];
+		const expected = [1, 3, 5, 7, 9];
+		const actual = mergeSorted(arr1, arr2);
+
+		expect(actual).toEqual(expected);
+	});
+
+	it(`should return a sorted array when arr2 is all greater than arr1`, () => {
+		const arr1 = [1, 2, 3, 4, 5];
+		const arr2 = [6, 7, 8, 9, 10];
+		const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		const actual = mergeSorted(arr1, arr2);
+
+		expect(actual).toEqual(expected);
+	});
+
+	it(`should return a sorted array when arrays have different length`, () => {
+		const arr1 = [1, 3, 5, 7, 9, 11, 12, 13];
+		const arr2 = [2, 4, 6, 8, 10];
+		const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 		const actual = mergeSorted(arr1, arr2);
 
 		expect(actual).toEqual(expected);
--- bootcamp/merge-sorted.js
+++ bootcamp/merge-sorted.js
@@ -1,7 +1,26 @@
 // Write code to merge two sorted arrays into a new sorted array
 
 const mergeSorted = (arr1, arr2) => {
-    
+  let output = [];
+
+  while (arr1.length > 0 || arr2.length > 0) {
+    if (arr1.length === 0) {
+      output = [...arr2, ...output];
+      arr2 = [];
+    } else if (arr2.length === 0) {
+      output = [...arr1, ...output];
+      arr1 = [];
+    } else {
+      if (arr1[arr1.length - 1] >= arr2[arr2.length - 1]) {
+        const num = arr1.pop();
+        output = [num, ...output];
+      } else {
+        const num = arr2.pop();
+        output = [num, ...output];
+      }
+    }
+  }
+  return output;
 };
 
 export default mergeSorted
