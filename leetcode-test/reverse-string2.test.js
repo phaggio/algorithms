abccb1cf2768a4e8a0e00222e78f3f74f7601c91
phaggio <richardchacha@hotmail.com>
2020-07-26 23:54:55 -0700

    reverse-string2

---
+++ leetcode-test/reverse-string2.test.js
@@ -0,0 +1,49 @@
+import reverseStr from '../leetcode/reverse-string2';
+
+describe('reverseStr()', () => {
+
+  it(`should return "bacdfeg" for "abcdefg", k = 2`, () => {
+    const s = "abcdefg";
+    const k = 2
+    const expected = "bacdfeg";
+    const actual = reverseStr(s, k);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should return 'cbadefhg' for 'abcdefgh'`, () => {
+    const s = 'abcdefgh';
+    const k = 3
+    const expected = 'cbadefhg';
+    const actual = reverseStr(s, k);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should return 'dcbaefghlkji' for 'abcdefghijkl'`, () => {
+    const s = 'abcdefghijkl';
+    const k = 4
+    const expected = 'dcbaefghlkji';
+    const actual = reverseStr(s, k);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should return 'abcdefgh' for 'abcdefgh'`, () => {
+    const s = 'abcdefgh';
+    const k = 1
+    const expected = 'abcdefgh';
+    const actual = reverseStr(s, k);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should return 'cba' for 'abc'`, () => {
+    const s = 'abc';
+    const k = 3;
+    const expected = 'cba';
+    const actual = reverseStr(s, k);
+
+    expect(actual).toEqual(expected);
+  });
+});
