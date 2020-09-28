c58c1353e91178d55c951bc4fde3dd0b900e31df
phaggio <richardchacha@hotmail.com>
2020-09-27 22:48:00 -0700

    selection algo

--- sort-test/selection.test.js
+++ sort-test/selection.test.js
@@ -1,10 +1,10 @@
-import bubble from '../sort/bubble';
+import selection from '../sort/selection';
 
-describe('bubble()', () => {
+describe('selection()', () => {
   it('should return a sorted array', () => {
     const arr = [1, 3, 2, 5, 9, 8, 7, 10, 6, 4];
     const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const actual = bubble(arr);
+    const actual = selection(arr);
 
     expect(actual).toEqual(expected);
   });
@@ -12,7 +12,7 @@ describe('bubble()', () => {
   it('should return a sorted array', () => {
     const arr = [1, 3, 2, 5, 9, 8, 1, 3, 2, 5, 9, 8, 7, 10, 6, 4, 7, 10, 6, 4];
     const expected = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
-    const actual = bubble(arr);
+    const actual = selection(arr);
 
     expect(actual).toEqual(expected);
   });
---
+++ sort/selection.js
@@ -0,0 +1,14 @@
+const selection = arr => {
+  for (let i = 0; i < arr.length; ++i) {
+    let lowestNumIndex = i
+    for (let j = i + 1; j < arr.length; ++j) {
+      if (arr[j] <= arr[lowestNumIndex]) lowestNumIndex = j
+    }
+    const temp = arr[i]
+    arr[i] = arr[lowestNumIndex]
+    arr[lowestNumIndex] = temp
+  }
+  return arr
+}
+
+export default selection
